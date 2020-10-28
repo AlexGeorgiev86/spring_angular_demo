622a04851639b0e5ab66909fb9d89b872e228cb9
AlexGeorgiev86 <hankyz@gmail.com>
2020-10-28 13:53:58 +0200

    Initial commit

---
+++ src/main/java/spring_angular_demo/SpringAngularDemoApplication.java
@@ -0,0 +1,13 @@
+package spring_angular_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring_angular_demo/SpringAngularDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_angular_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
